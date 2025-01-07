91a3b029e32cf09bc5ec1bcd4bb5de5e87888e7e
Eric Ye <yuxuan.smiley@gmail.com>
2025-01-06 18:21:33 -0800

    Add extra info

--- craftax/craftax_classic/envs/common.py
+++ craftax/craftax_classic/envs/common.py
@@ -1,4 +1,5 @@
-from craftax.craftax_classic.envs.craftax_state import EnvState
+from craftax.craftax_classic.envs.craftax_state import EnvState, Inventory
+from dataclasses import fields
 from craftax.craftax_classic.constants import *
 from jax import Array
 
@@ -9,6 +10,20 @@ def compute_score(state: EnvState, done: Array):
     for achievement in Achievement:
         name = f"Achievements/{achievement.name.lower()}"
         info[name] = achievements[achievement.value]
+
+    for inventory in fields(Inventory):
+        info["Inventory/" + inventory.name] = getattr(state.inventory, inventory.name)
+
+    info["health"] = state.player_health
+    info["food"] = state.player_food
+    info["drink"] = state.player_drink
+    info["energy"] = state.player_energy
+    info["sleeping"] = state.is_sleeping
+    info["recover"] = state.player_recover
+    info["hunger"] = state.player_hunger
+    info["thirst"] = state.player_thirst
+    info["fatigure"] = state.player_fatigue
+
     # Geometric mean with an offset of 1%
     info["score"] = jnp.exp(jnp.mean(jnp.log(1 + achievements))) - 1.0
     return info
